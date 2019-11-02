23779109398ae2811cff3c17b6ab46d2d8a11aa3
Adam <adam.bates.17@cnu.edu>
2019-11-02 11:52:52 -0400

    Finished fileIO

--- FileIO.cpp
+++ FileIO.cpp
@@ -6,19 +6,60 @@
  */
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
 
 using namespace std;
 
+
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	//declare ifstream and string to hold values
+	ifstream file_to_open;
+	string line;
+
+	//open file, return fail constant if cannot open
+	file_to_open.open(filename.c_str());
+	if (!file_to_open.is_open()){
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+
+	//while not end of file, use line to add contents to 'contents'
+	while (!file_to_open.eof()){
+		getline(file_to_open, line);
+		contents+=line;
+	}
+
+	//close file and return success constant
+	file_to_open.close();
+	return SUCCESS;
 }
 
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	//declare ofstream and vector iterator
+	ofstream file_to_open;
+	vector<string>::iterator vector_iterator;
+
+	//open file, return fail constant if cannot open
+	file_to_open.open(filename.c_str());
+	if (!file_to_open.is_open()){
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+	}
+
+	//iterate through vector using vector_iterator
+	for(vector_iterator = myEntryVector.begin();
+			vector_iterator != myEntryVector.end();
+			vector_iterator++){
+		//write data to file
+		file_to_open << *vector_iterator << endl;
+	}
+
+	//close file and return success constant
+	file_to_open.close();
+	return SUCCESS;
 }
 
 
